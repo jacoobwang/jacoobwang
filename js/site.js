0403ff85a46c77221c5a9ebea4533c6987b779d9
jacoobwang <531532957@qq.com>
2017-03-20 16:05:12 +0800

    publish v1

---
+++ js/site.js
@@ -0,0 +1,49 @@
+function fixSizes() {
+    var t = $(window).height()
+      , a = $(window).width();
+    if ($(".fullscreen").css("height", t),
+    a > 960) {
+        $(".home, .home .background, .home .container").css("height", 770);
+        var e = 650 - $(".home .info").height();
+        $(".home .info").css("bottom", e / 2),
+        $(".home .info").css("right", 0)
+    } else
+        $(".home .info").css("margin-top", ($(".home .info").parent().height() - $(".home .info").height()) / 2);
+    $(".vertical-center").each(function() {
+        $(this).css("margin-top", ($(this).parent().height() - $(this).height()) / 2)
+    })
+}
+
+jQuery(document).ready(function($$) {
+    "use strict";
+    $$(document).scroll(function() {
+        $$(this).scrollTop();
+        $$(this).scrollTop() > 100 ? $$("header").stop().animate({
+            backgroundColor: "rgba(0,0,0,1)"
+        }, "fast") : $$("header").stop().animate({
+            backgroundColor: "rgba(0,0,0,0)"
+        }, "fast")
+    });
+})
+
+$(window).load(function() {
+    //fixSizes();
+
+    $(this).scrollTop();
+    $(this).scrollTop() > 100 ? $("header").stop().animate({
+        backgroundColor: "rgba(0,0,0,1)"
+    }, "fast") : $("header").stop().animate({
+        backgroundColor: "rgba(0,0,0,0)"
+    }, "fast");
+    
+    $(".loader").delay(1e3).fadeOut("slow");
+    $("body").css("overflow", "auto");
+
+    setTimeout(function() {
+        $("header").addClass("animated fadeInDown")
+    }, 1300);
+
+    // setTimeout(function() {
+    //     $(".welcome").addClass("tracking-in-expand")
+    // }, 1500)
+})
